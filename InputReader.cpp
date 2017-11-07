94d7dd7b714c18124d72c1e5cd3b1a21667c14cf
Tomer Amir <tomer.a@hotmail.com>
2017-11-07 15:37:12 +0200

    First commit with some stubs

---
+++ InputReader.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Tomer Amir on 07/11/2017.
+//
+
+#include "InputReader.h"
+
+PazzlePeice *InputReader::readInput(string path) {
+    string res = "1 0 0 0 0\n";
+    return nullptr;
+}
