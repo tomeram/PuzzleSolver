aeebb9de2136ace561d00f205cabfbfd07f0fe98
Tomer Amir <tomer.a@hotmail.com>
2017-11-07 15:54:15 +0200

    Added CMake stuff

---
+++ cmake-build-debug/CMakeFiles/PazzleSolver.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PazzleSolver.dir/main.cpp.o"
+  "CMakeFiles/PazzleSolver.dir/PazzlePeice.cpp.o"
+  "CMakeFiles/PazzleSolver.dir/InputReader.cpp.o"
+  "PazzleSolver.pdb"
+  "PazzleSolver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PazzleSolver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/PazzleSolver.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PazzleSolver.
+# This may be replaced when dependencies are built.
