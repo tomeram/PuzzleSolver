9063e0a27aec963c207acd566302f4a316877e8b
Tomer Amir <tomer.a@hotmail.com>
2017-12-13 17:03:28 +0200

    More simplification and refactoring

--- src/RotationSolver.cpp
+++ src/RotationSolver.cpp
@@ -18,87 +18,89 @@ int RotationSolver::getPair(int p) {
 	return -p;
 }
 
-bool RotationSolver::fillRow(int rowSize, int colSize, int row , int col_left , int col_right, RotationSolution sol, TypesMap &typesMap) {
-	if (col_left > col_right) {
+bool RotationSolver::fillRow(int rowSize, int colSize, int row, int col_left, RotationSolution sol, TypesMap &typesMap) {
+	if (col_left > col_left + rowSize) {
 		return true;
-	} else {
-		TypesMap::Constraints c;
-
-		if (col_left-1 > 0) {
-			PuzzlePiece* myLeft = sol[col_left-1][row];
-			if (myLeft == nullptr) {
-				c.left(-2);
-			} else {
-				c.left(getPair(myLeft->left()));
-			}
-
-		} else {
-			c.left(0);
-		}
-
-		if (row-1 > 0) {
-			PuzzlePiece* myTop = sol[col_left][row-1];
-			if (myTop== nullptr) {
-				c.top(-2);
-			} else {
-				c.top(getPair(myTop->top()));
-			}
-		} else {
-			c.top(0);
-		}
-
-		if (row+1 < rowSize) {
-			PuzzlePiece* myBottom = sol[col_left][row+1];
-			if (myBottom== nullptr) {
-				c.bottom(-2);
-			} else {
-				c.bottom(getPair(myBottom->bottom()));
-			}
-		} else {
-			c.bottom(0);
-		}
-
-		if (col_left+1 < colSize) {
-			PuzzlePiece* myright = sol[col_left+1][row];
-			if (myright== nullptr) {
-				c.right(-2);
-			} else {
-				c.right(getPair(myright->right()));
-			}
-		} else {
-			c.right(0);
-		}
-
-		auto list =  typesMap.getTypes(c);
-
-		for (auto type : list) {
-			TypesMap newMap = typesMap;
-			PuzzlePiece* piece = newMap.getPiece(type);
-			if (piece != nullptr) {
-				sol[col_left][row]= piece;
-				bool b= fillRow(rowSize, colSize, row , col_left+1 , col_right , sol, newMap);
-				if (b == true) {
-					return true;
-				}
-				else{
-					sol[col_left][row]= nullptr;
-				}
-			}
-		}
-		return false;
 	}
+
+	TypesMap::Constraints c;
+
+	if (col_left-1 > 0) {
+		PuzzlePiece* myLeft = sol[col_left-1][row];
+		if (myLeft == nullptr) {
+			c.left(-2);
+		} else {
+			c.left(getPair(myLeft->left()));
+		}
+
+	} else {
+		c.left(0);
+	}
+
+	if (row-1 > 0) {
+		PuzzlePiece* myTop = sol[col_left][row-1];
+		if (myTop == nullptr) {
+			c.top(-2);
+		} else {
+			c.top(getPair(myTop->top()));
+		}
+	} else {
+		c.top(0);
+	}
+
+	if (row+1 < rowSize) {
+		PuzzlePiece* myBottom = sol[col_left][row+1];
+		if (myBottom == nullptr) {
+			c.bottom(-2);
+		} else {
+			c.bottom(getPair(myBottom->bottom()));
+		}
+	} else {
+		c.bottom(0);
+	}
+
+	if (col_left+1 < colSize) {
+		PuzzlePiece* myright = sol[col_left+1][row];
+		if (myright == nullptr) {
+			c.right(-2);
+		} else {
+			c.right(getPair(myright->right()));
+		}
+	} else {
+		c.right(0);
+	}
+
+	auto goodTypes =  typesMap.getTypes(c);
+
+	for (const auto &type: goodTypes) {
+		TypesMap newMap = typesMap;
+		PuzzlePiece* piece = newMap.getPiece(type);
+
+		if (piece != nullptr) {
+			sol[col_left][row]= piece;
+
+			if (fillRow(rowSize, colSize, row, col_left + 1, sol, newMap)) {
+				return true;
+			}
+			else{
+				sol[col_left][row] = nullptr;
+			}
+		}
+	}
+
+	return false;
 }
 
 
-bool RotationSolver::topRow(int rowSize, int colSize, int row_top , int col_left , int col_right, RotationSolution sol, TypesMap &typesMap) {
-	return fillRow(rowSize, colSize, row_top , col_left , col_right, sol, typesMap);
+bool RotationSolver::topRow(int rowSize, int colSize, int row_top, int col_left, RotationSolution sol, TypesMap &typesMap) {
+	return fillRow(rowSize, colSize, row_top, col_left, sol, typesMap);
 }
 
-bool RotationSolver::bottomRow(int rowSize, int colSize, int row_bott , int col_left , int col_right, RotationSolution sol, TypesMap &typesMap) {
-	return fillRow(rowSize, colSize, row_bott , col_left , col_right, sol, typesMap);
+bool RotationSolver::bottomRow(int rowSize, int colSize, int row_bott, int col_left, RotationSolution sol, TypesMap &typesMap) {
+	return fillRow(rowSize, colSize, row_bott, col_left, sol, typesMap);
 }
 
-bool RotationSolver::fillCol(int rowSize, int colSize, int col , int row_top , int row_bott, RotationSolution sol, TypesMap &typesMap) {
+bool RotationSolver::fillCol(int rowSize, int colSize, int col, int row_top, int row_bott, RotationSolution sol, TypesMap &typesMap) {
 	if (row_top > row_bott) {
 		return true;
 	}
@@ -106,7 +108,7 @@ bool RotationSolver::fillCol(int rowSize, int colSize, int col , int row_top , i
 		TypesMap::Constraints c;
 		if (col-1 > 0) {
 			PuzzlePiece* myLeft = sol[col-1][row_top];
-			if (myLeft== nullptr) {
+			if (myLeft == nullptr) {
 				c.left(-2);
 			} else {
 				c.left(getPair(myLeft->left()));
@@ -118,7 +120,7 @@ bool RotationSolver::fillCol(int rowSize, int colSize, int col , int row_top , i
 
 		if (col+1 < colSize) {
 			PuzzlePiece* myTop = sol[col+1][row_top];
-			if (myTop== nullptr) {
+			if (myTop == nullptr) {
 				c.top(-2);
 			} else {
 				c.top(getPair(myTop->top()));
@@ -129,7 +131,7 @@ bool RotationSolver::fillCol(int rowSize, int colSize, int col , int row_top , i
 
 		if (row_top+1 < rowSize) {
 			PuzzlePiece* myBottom = sol[col][row_top+1];
-			if (myBottom== nullptr) {
+			if (myBottom == nullptr) {
 				c.bottom(-2);
 			} else {
 				c.bottom(getPair(myBottom->bottom()));
@@ -140,7 +142,7 @@ bool RotationSolver::fillCol(int rowSize, int colSize, int col , int row_top , i
 
 		if (row_top-1 > 0) {
 			PuzzlePiece* myright = sol[col][row_top-1];
-			if (myright== nullptr) {
+			if (myright == nullptr) {
 				c.right(-2);
 			} else {
 				c.right(getPair(myright->right()));
@@ -151,13 +153,13 @@ bool RotationSolver::fillCol(int rowSize, int colSize, int col , int row_top , i
 
 		auto list = typesMap.getTypes(c);
 		if (! list.empty()) {
-			for (auto type : list) {
+			for (const auto &type: list) {
 				TypesMap newMap = typesMap;
 				PuzzlePiece* piece = newMap.getPiece(type);
 				if (piece != nullptr) {
 					sol[col][row_top]= piece;
-					bool b= fillCol(rowSize, colSize, col , row_top+1 , row_bott , sol, newMap);
-					if (b == true) {
+					bool b= fillCol(rowSize, colSize, col, row_top+1, row_bott, sol, newMap);
+					if (b) {
 						return true;
 					}
 					else{
@@ -173,72 +175,77 @@ bool RotationSolver::fillCol(int rowSize, int colSize, int col , int row_top , i
 	}
 }
 
-bool RotationSolver::leftCol(int rowSize, int colSize, int col_left , int row_top , int row_bott, RotationSolution sol, TypesMap &typesMap) {
-	return fillCol(rowSize, colSize, col_left , row_top , row_bott, sol, typesMap);
+bool RotationSolver::leftCol(int rowSize, int colSize, int col_left, int row_top, int row_bott, RotationSolution sol, TypesMap &typesMap) {
+	return fillCol(rowSize, colSize, col_left, row_top, row_bott, sol, typesMap);
 }
 
 bool RotationSolver::rightCol(int rowSize, int colSize, int col_right, int row_top, int row_bott,
-															RotationSolution sol, TypesMap &typesMap) {
-	return fillCol(rowSize, colSize, col_right , row_top , row_bott, sol, typesMap);
+							  RotationSolution sol, TypesMap &typesMap) {
+	return fillCol(rowSize, colSize, col_right, row_top, row_bott, sol, typesMap);
 }
 
-bool RotationSolver::fillFrames(int rowSize, int colSize , int row_top, int row_bott,
-																int col_left, int col_right, RotationSolution sol, TypesMap &typesMap) {
+bool RotationSolver::fillFrames(int rowSize, int colSize, int row_top, int row_bott,
+								int col_left, int col_right, RotationSolution sol, TypesMap &typesMap) {
 
 	if (col_left > col_right && row_top > row_bott) {
 		return true;
-
 	}
 
 	if (col_left < col_right && row_top < row_bott) {
-		if (topRow(rowSize, colSize, row_top , col_left , col_right, sol, typesMap) &&
-				bottomRow(rowSize, colSize, row_bott , col_left , col_right, sol, typesMap) &&
-				rightCol(rowSize, colSize, col_right, row_top, row_bott, sol, typesMap) &&
-				leftCol(rowSize, colSize, col_left , row_top , row_bott, sol, typesMap)) {
-			fillFrames(rowSize, colSize, row_top + 1, row_bott - 1, col_left +1 , col_right -1 , sol, typesMap);
+		if (topRow(rowSize, colSize, row_top, col_left, sol, typesMap) &&
+			bottomRow(rowSize, colSize, row_bott, col_left, sol, typesMap) &&
+			rightCol(rowSize, colSize, col_right, row_top, row_bott, sol, typesMap) &&
+			leftCol(rowSize, colSize, col_left, row_top, row_bott, sol, typesMap))
+		{
+			fillFrames(rowSize, colSize, row_top + 1, row_bott - 1, col_left +1, col_right -1, sol, typesMap);
 		} else {
 			return false;
 		}
 	}
 
 	if (col_left == col_right && row_top < row_bott) {
-		if (topRow(rowSize, colSize, row_top , col_left , col_right, sol, typesMap) &&
-				bottomRow(rowSize, colSize, row_bott , col_left , col_right, sol, typesMap) &&
-				leftCol(rowSize, colSize, col_left , row_top , row_bott, sol, typesMap)) {
-			fillFrames(rowSize, colSize, row_top + 1, row_bott - 1, col_left +1 , col_right, sol, typesMap);
+		if (topRow(rowSize, colSize, row_top, col_left, sol, typesMap) &&
+			bottomRow(rowSize, colSize, row_bott, col_left, sol, typesMap) &&
+			leftCol(rowSize, colSize, col_left, row_top, row_bott, sol, typesMap))
+		{
+			fillFrames(rowSize, colSize, row_top + 1, row_bott - 1, col_left +1, col_right, sol, typesMap);
 		} else {
 			return false;
 		}
 	}
 	if (col_left < col_right && row_top == row_bott) {
-		if (leftCol(rowSize, colSize, col_left , row_top , row_bott, sol, typesMap) &&
-				rightCol(rowSize, colSize, col_right, row_top, row_bott, sol, typesMap) &&
-				topRow(rowSize, colSize, row_top , col_left , col_right, sol, typesMap)) {
-			fillFrames(rowSize, colSize, row_top + 1, row_bott , col_left +1 , col_right -1, sol, typesMap);
+		if (leftCol(rowSize, colSize, col_left, row_top, row_bott, sol, typesMap) &&
+			rightCol(rowSize, colSize, col_right, row_top, row_bott, sol, typesMap) &&
+			topRow(rowSize, colSize, row_top, col_left, sol, typesMap))
+		{
+			fillFrames(rowSize, colSize, row_top + 1, row_bott, col_left +1, col_right -1, sol, typesMap);
 		} else {
 			return false;
 		}
 	}
 	if (col_left == col_right && row_top == row_bott) {
-		if (topRow(rowSize, colSize, row_top , col_left , col_right, sol, typesMap) &&
-				leftCol(rowSize, colSize, col_left , row_top , row_bott, sol, typesMap)) {
-			fillFrames(rowSize, colSize, row_top + 1, row_bott , col_left +1 , col_right, sol, typesMap);
+		if (topRow(rowSize, colSize, row_top, col_left, sol, typesMap) &&
+			leftCol(rowSize, colSize, col_left, row_top, row_bott, sol, typesMap))
+		{
+			fillFrames(rowSize, colSize, row_top + 1, row_bott, col_left +1, col_right, sol, typesMap);
 		} else {
 			return false;
 		}
 	}
 	if (col_left < col_right && row_top > row_bott) {
-		if (leftCol(rowSize, colSize, col_left , row_top , row_bott, sol, typesMap) &&
-				rightCol(rowSize, colSize, col_right, row_top, row_bott, sol, typesMap)) {
-			fillFrames(rowSize, colSize, row_top, row_bott , col_left +1 , col_right -1, sol, typesMap);
+		if (leftCol(rowSize, colSize, col_left, row_top, row_bott, sol, typesMap) &&
+			rightCol(rowSize, colSize, col_right, row_top, row_bott, sol, typesMap))
+		{
+			fillFrames(rowSize, colSize, row_top, row_bott, col_left +1, col_right -1, sol, typesMap);
 		} else {
 			return false;
 		}
 	}
 	if (col_left > col_right && row_top < row_bott) {
-		if (topRow(rowSize, colSize, row_top , col_left , col_right, sol, typesMap) &&
-				bottomRow(rowSize, colSize, row_bott , col_left , col_right, sol, typesMap)) {
-			fillFrames(rowSize, colSize, row_top+1, row_bott-1 , col_left , col_right, sol, typesMap);
+		if (topRow(rowSize, colSize, row_top, col_left, sol, typesMap) &&
+			bottomRow(rowSize, colSize, row_bott, col_left, sol, typesMap))
+		{
+			fillFrames(rowSize, colSize, row_top+1, row_bott-1, col_left, col_right, sol, typesMap);
 		} else {
 			return false;
 		}
--- src/RotationSolver.h
+++ src/RotationSolver.h
@@ -14,17 +14,18 @@ using namespace std;
 
 class RotationSolver {
 private:
-	RotationSolution sol;
+	RotationSolution _sol;
+	bool _rotate = false;
 
 	int getPair(int p);
-	bool fillRow(int rowSize, int colSize, int row , int col_left , int col_right, RotationSolution sol, TypesMap &typesMap);
-	bool topRow(int rowSize, int colSize, int row_top , int col_left , int col_right, RotationSolution sol, TypesMap &typesMap);
-	bool bottomRow(int rowSize, int colSize, int row_bott , int col_left , int col_right, RotationSolution sol, TypesMap &typesMap);
-	bool fillCol(int rowSize, int colSize, int col , int row_top , int row_bott, RotationSolution sol, TypesMap &typesMap);
-	bool leftCol(int rowSize, int colSize, int col_left , int row_top , int row_bott, RotationSolution sol, TypesMap &typesMap);
+	bool fillRow(int rowSize, int colSize, int row, int col_left, RotationSolution sol, TypesMap &typesMap);
+	bool topRow(int rowSize, int colSize, int row_top, int col_left, RotationSolution sol, TypesMap &typesMap);
+	bool bottomRow(int rowSize, int colSize, int row_bott, int col_left, RotationSolution sol, TypesMap &typesMap);
+	bool fillCol(int rowSize, int colSize, int col, int row_top, int row_bott, RotationSolution sol, TypesMap &typesMap);
+	bool leftCol(int rowSize, int colSize, int col_left, int row_top, int row_bott, RotationSolution sol, TypesMap &typesMap);
 	bool rightCol(int rowSize, int colSize, int col_right, int row_top, int row_bott,
 				  RotationSolution sol, TypesMap &typesMap);
-	bool fillFrames(int rowSize, int colSize , int row_top, int row_bott,
+	bool fillFrames(int rowSize, int colSize, int row_top, int row_bott,
 					int col_left, int col_right, RotationSolution sol, TypesMap &typesMap);
 public:
 	RotationSolver();
