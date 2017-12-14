c178ebb90c2af8784096358f4601f54c9ad12e78
Tomer Amir <tomer.a@hotmail.com>
2017-12-14 17:26:22 +0200

    Bug fixes, and added possible frame sizes check

--- src/RotationSolver.cpp
+++ src/RotationSolver.cpp
@@ -76,7 +76,7 @@ bool RotationSolver::fillFrames(int rowSize, int colSize, int corner, int col, i
 			continue;
 		}
 
-		_sol[col][row] = piece;
+		_sol[row][col] = piece;
 
 		// TODO: Export to another function
 		int newCol = -1, newRow = -1;
@@ -88,7 +88,7 @@ bool RotationSolver::fillFrames(int rowSize, int colSize, int corner, int col, i
 			if (col < lastCol) {
 				newCol = col + 1;
 				newRow = row;
-			} else {
+			} else if (row < lastRow) {
 				newCol = col;
 				newRow = row + 1;
 			}
@@ -100,7 +100,7 @@ bool RotationSolver::fillFrames(int rowSize, int colSize, int corner, int col, i
 				newCol = col - 1;
 				newRow = row;
 			}
-		} else if (col == lastRow) {
+		} else if (row == lastRow) {
 			if (col > corner) {
 				newCol = col - 1;
 				newRow = row;
@@ -109,33 +109,70 @@ bool RotationSolver::fillFrames(int rowSize, int colSize, int corner, int col, i
 				newRow = row - 1;
 			}
 		} else {
-			if (row > corner) {
+			if (row > corner + 1) {
 				newCol = col;
 				newRow = row - 1;
 			}
 		}
 
-		if (newCol < 0) {
+		if (newCol < 0 || (newRow == corner && newCol == corner)) {
 			int newIndex = corner + 1;
 
 			auto min = std::min(_sol.get_height(), _sol.get_width());
 
 			min = (min / 2) + (min % 2);
 
-			if (corner < min) {
-				if (fillFrames(rowSize, colSize, newIndex, newIndex, newIndex, typesMap)) {
-					return true;
-				}
+			if (newIndex == min || fillFrames(rowSize - 2, colSize - 2, newIndex, newIndex, newIndex, typesMap)) {
+				return true;
 			}
 		} else if (fillFrames(rowSize, colSize, corner, newCol, newRow, typesMap)) {
 			return true;
 		}
 
 		typesMap.addPiece(piece);
-		_sol[col][row] = nullptr;
+		_sol[row][col] = nullptr;
 	}
 
 	return false;
+}
+
+// TODO: Add rotation support
+bool checkEdgesR(const vector<PuzzlePiece> &pieces, set<int> &rowLengths)
+{
+	int l = 0, t = 0, r = 0, b = 0;
+
+	for (auto p: pieces) {
+		if (p.left() == 0) {
+			l++;
+		}
+
+		if (p.top() == 0) {
+			t++;
+		}
+
+		if (p.right() == 0) {
+			r++;
+		}
+
+		if (p.bottom() == 0) {
+			b++;
+		}
+
+	}
+
+	auto newRows = rowLengths;
+
+	for (int rows: rowLengths) {
+		int cols = pieces.size() / rows;
+
+		if (l < cols || r < cols || t < rows || b < rows) {
+			newRows.erase(rows);
+		}
+	}
+
+	rowLengths = newRows;
+
+	return !newRows.empty();
 }
 
 RotationSolver::RotationSolver(Puzzle *puzzle, bool _rotate) : _puzzle(puzzle), _rotate(_rotate)
@@ -149,6 +186,7 @@ RotationSolver::RotationSolver(Puzzle *puzzle, bool _rotate) : _puzzle(puzzle), 
 	}
 
 	// TODO: Basic input checks
+	checkEdgesR(puzzle->getPieces(), _rowLengths);
 }
 
 bool RotationSolver::solve() {
